6685295d5510cf92d1c379400bb3efc10862c740
choushen <jacob.mckenzie@icloud.com>
2024-01-30 23:34:53 +0000

    Added a comment to with links to the app i'll be testing

--- test-fw/src/test/java/test_cases/UserTests.java
+++ test-fw/src/test/java/test_cases/UserTests.java
@@ -3,14 +3,22 @@ package test_cases;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UserTests {
 
     @Test
     public void registerToTheApplication () {
 
-        WebDriver _driver = new ChromeDriver();
+        // Site I'll be testing against
+        // https://documenter.getpostman.com/view/4012288/TzK2bEa8#intro
+        // https://thinking-tester-contact-list.herokuapp.com/
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        WebDriver _driver = new ChromeDriver(options);
         _driver.get("https://mvnrepository.com/artifact/junit/junit/4.13.2");
+        _driver.quit();
 
     }
 
